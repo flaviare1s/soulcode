03d268f44c9e8e916afdcda23538e6f60eb3f25e
flaviare1s <flaviarster@gmail.com>
2024-06-25 17:49:41 -0300

    Update figura.js

--- etapa-1/javascript/aula2506/figura.js
+++ etapa-1/javascript/aula2506/figura.js
@@ -1,7 +1,17 @@
-for (let i = 1; i <= 5; i++) {
-  let figura = ''
-  for (let j = 1; j <= i; j++) {
-    figura += '*'
-  }
-  console.log(figura)
+// for (let i = 1; i <= 5; i++) {
+//   let figura = ''
+//   for (let j = 1; j <= i; j++) {
+//     figura += '*'
+//   }
+//   console.log(figura)
+// }
+
+// Outra forma de fazer: 
+
+let linha = ''
+let altura = 5
+
+for (let i = 1; i <= altura; i++) {
+  linha += '❤️'
+  console.log(linha)
 }
