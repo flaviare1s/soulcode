2b402515f9f4c4c81e89780a1bffa052db01230d
flaviare1s <flaviarster@gmail.com>
2024-06-25 17:15:00 -0300

    Mais exercícios

---
+++ etapa-1/javascript/aula2506/tabuada.js
@@ -0,0 +1,8 @@
+for (let nTab = 1; nTab <= 10; nTab++) {
+  console.log('----------------')
+  console.log(`Tabuada do ${nTab}`)
+  console.log('----------------')
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${nTab} x ${i} = ${nTab * i}`)
+  }
+}
